933d6e767743f879a7fe932fbefae4bf83b3eadc
Wei Liu <liuwei_seu@qq.com>
2023-05-15 19:34:52 -0700

    the arch is based on dict now.

--- workunit.py
+++ workunit.py
@@ -22,11 +22,13 @@ class xml(object):
             if(isinstance(v,list)):
                 self.print_xml_begin(k)
                 for i in v:
+                    #ins = xml(k,v)
                     i.print_xml(self.f)
                 self.print_xml_end(k)
-            elif(isinstance(v,xml)):
+            elif(isinstance(v,dict)):
+                ins = xml(k,v)
                 self.print_xml_begin(k)
-                v.print_xml(self.f)
+                ins.print_xml(self.f)
                 self.print_xml_end(k)
             else:
                 self.print_xml_indent()
@@ -47,36 +49,75 @@ class xml(object):
 """
 tape example
 """
-tape_info = {'name'             : 'test', \
-             'start_time'       : 2454822.5698634, \
-             'last_block_time'  : 2454822.5698634, \
-             'last_block_done'  : 6280, \
-             'missed'           : 0, \
-             'tape_quality'     : 0,
-             'beam'             : 8}
+tape_info = {
+    'name'             : 'test', \
+    'start_time'       : 2454822.5698634, \
+    'last_block_time'  : 2454822.5698634, \
+    'last_block_done'  : 6280, \
+    'missed'           : 0, \
+    'tape_quality'     : 0,
+    'beam'             : 8
+ }
 
 
 """
 coordinate example
 """
-coordinate_t = {'time'      : 2454822.5698622, \
-                'ra'            : 3.2887318938763, \
-                'dec'           : 23.410418247457}
+coordinate_t = {
+    'time'          : 2454822.5698622, \
+    'ra'            : 3.2887318938763, \
+    'dec'           : 23.410418247457
+}
+
 """
 data_description example
 """ 
 coordinate_ins = xml('coordinate_t', coordinate_t)
-data_desc =  {'start_ra'         : 3.2887318938763, \
-            'start_dec'        : 23.410418247457, \
-            'end_ra'           : 3.3185884614727, \
-            'end_dec'          : 26.095076667942, \
-            'true_angle_range' : 2.7152905955851, \
-            'time_recorded'    : 'Mon Dec 22 01:40:36 2008', \
-            'time_recorded_jd' : 2454822.5698622, \
-            'nsamples'         : 1048576, \
-            'coords'           : [coordinate_ins,coordinate_ins]}
+data_desc =  {
+    'start_ra'        : 3.2887318938763, \
+    'start_dec'         : 23.410418247457, \
+    'end_ra'            : 3.3185884614727, \
+    'end_dec'           : 26.095076667942, \
+    'true_angle_range'  : 2.7152905955851, \
+    'time_recorded'     : 'Mon Dec 22 01:40:36 2008', \
+    'time_recorded_jd'  : 2454822.5698622, \
+    'nsamples'          : 1048576, \
+    'coords'            : [coordinate_ins, coordinate_ins]
+}
 
 """
-class receiver_cfg
+receiver example
 """
-#receiver_cfg = {'s4_id' = 
+receiver_cfg = {
+    's4_id'         : 11, \
+    'name'          : 'Arecibo 1.4GHz Array, Beam 4, Pol 0', \
+    'beam_width'    : 0.0500000007, \
+    'center_freq'   : 1420, \
+    'latitude'      : 18.3538056, \
+    'longitude'     : -66.7552222, \
+    'elevation'     : 497, \
+    'diameter'      : 168, \
+    'az_orientation': 180, \
+    'az_corr_coeff' : -37, \
+    'zen_corr_coeff': -57, \
+    'array_az_ellipse' : 329.06, \
+    'array_za_ellipse' : 384.005, \
+    'array_angle'   : -60
+}
+
+
+"""
+group_info example
+"""
+group_info = {
+    'tape_info'     : tape_info, \
+    'name'          : '', \
+    'data_desc'     : data_desc
+}
+"""
+workunit_header example
+"""
+workunit_header = {
+    'name' : '', \
+    'group_info' : group_info
+}
--- workunit_generator.py
+++ workunit_generator.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 import sys, os
 
 sys.path.insert(0, './')
@@ -6,13 +8,19 @@ from workunit import xml
 from workunit import tape_info
 from workunit import data_desc
 from workunit import coordinate_t
+from workunit import group_info
+from workunit import workunit_header
 
 if __name__ == '__main__':
     f = open('test.sah','w')
-    
+    """
     tape_info_ins = xml('tape_info',tape_info)
     tape_info_ins.print_xml(f)
     data_desc_ins = xml('data_desc', data_desc)
     data_desc_ins.print_xml(f)
-
+    group_info_ins = xml('group_info', group_info)
+    group_info_ins.print_xml(f)
+    """
+    workunit_header_ins = xml('work_header', workunit_header)
+    workunit_header_ins.print_xml(f)
     f.close()
